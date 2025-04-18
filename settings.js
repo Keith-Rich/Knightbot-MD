c5ea96b3738029bc57e77baa38ce4bc43ce8605f
Keith <123067066+Keith-Rich@users.noreply.github.com>
2025-04-19 01:05:13 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'ᴍʀ.ꜱᴜᴘᴇʀᴍᴀɴ', // Your name
-  ownerNumber: '254784900006', //Your number
+  ownerNumber: '254784900006', '254113328907', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
